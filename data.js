1995354a21dcd6380fff3ebe71df80b6f22a6e5c
Ngocthai2703web1s <159770013+Ngocthai2703web1s@users.noreply.github.com>
2026-04-02 09:13:45 +0700

    Update data.js

--- data.js
+++ data.js
@@ -27,7 +27,16 @@ window.bookData = [
 	{ category: "Trinh Thám", title: "Căn Hộ Số 203", link: "https://link4m.net/aPRenu0N" },
 	{ category: "Trinh Thám", title: "Bảy Ngày Đếm Ngược", link: "https://link4m.com/tvym9" },
 	{ category: "Trinh Thám", title: "Nếu Như Được Làm Lại", link: "https://link4m.com/fMCQvlg" },
-	{ category: "Công Nghệ", title: "BA Interview Questions and Answers", link: "#" }
+	{ category: "Công Nghệ", title: "BA Interview Questions and Answers", link: "https://link4m.net/6GtTMcFV" },
+	{ category: "Trinh Thám", title: "Vòng Xoáy Chết", link: "https://link4m.com/esZrB" },
+	{ category: "Trinh Thám", title: "Người Đàn Bà Trong Bóng Đêm", link: "https://link4m.com/TES1xCcq" },
+	{ category: "Trinh Thám", title: "Theo Em Vào Bóng Đêm", link: "https://link4m.com/BQ9xq" },
+	{ category: "Trinh Thám", title: "Cô Dâu Đen", link: "https://link4m.com/Dedn0Vg1" },
+	{ category: "Trinh Thám", title: "Người Gác Cây Long Não", link: "https://link4m.net/lnsU9" },
+	{ category: "Trinh Thám", title: "Tội Ác Ở Orcival", link: "https://link4m.net/ATgjEKp" },
+	{ category: "Trinh Thám", title: "Truy Lùng Tên Sát Nhân", link: "https://link4m.com/ju9qSYb" },
+	{ category: "Trinh Thám", title: "Bẫy Sói", link: "https://link4m.com/Ohe5y2" },
+	{ category: "Trinh Thám", title: "Cái Chết", link: "https://link4m.net/82Sk8C1B" }
 ];
 
 // Dữ liệu ảnh quảng cáo (adv)
@@ -39,7 +48,15 @@ window.imageData = [
     { src: 'https://down-zl-vn.img.susercontent.com/vn-11134207-81ztc-mlncw1u94c9b67.webp', name: 'Thùng 30 gói Khăn giấy rút TopGia', link: 'https://s.shopee.vn/70FW5jrPis' },
     { src: 'https://down-zl-vn.img.susercontent.com/vn-11134207-7r98o-lndsoahicvwa54.webp', name: 'Hộp đựng rau củ tủ lạnh có quai trong suốt', link: 'https://s.shopee.vn/9fGHGj90cJ' },
     { src: 'https://down-zl-vn.img.susercontent.com/sg-11134201-7repu-m8c771r43cmfdd.webp', name: 'Combo 03 Túi Đường Mía Thiên Nhiên Biên Hòa 1Kg', link: 'https://s.shopee.vn/6VJFV1WoJl' },
-    { src: 'https://down-zl-vn.img.susercontent.com/vn-11134207-7r98o-lza4c4tc6i4td6.webp', name: 'Trà Bổ Máu Ích Huyết Mát Gan Bình An', link: 'https://s.shopee.vn/5L7I6yLd4T' }
+    { src: 'https://down-zl-vn.img.susercontent.com/vn-11134207-7r98o-lza4c4tc6i4td6.webp', name: 'Trà Bổ Máu Ích Huyết Mát Gan Bình An', link: 'https://s.shopee.vn/5L7I6yLd4T' },
+	{ src: 'https://down-tx-vn.img.susercontent.com/vn-11134207-81ztc-mkkgyf8twl5067.webp', name: 'Combo 3 quần lót nam dáng Trunk Bamboo mềm mại, kháng khuẩn', link: 'https://s.shopee.vn/2VnPX9AqI1' },
+	{ src: 'https://down-tx-vn.img.susercontent.com/vn-11134207-81ztc-mm1fljoi17uta5.webp', name: 'Nước Súc Miệng Chuẩn Y Khoa DrKam 250ml - Khử Hôi Miệng Giảm Viêm Nướu - Không Cồn Không Cay', link: 'https://s.shopee.vn/7fVVgisw09' },
+	{ src: 'https://down-tx-vn.img.susercontent.com/vn-11134207-81ztc-mm9tw63jdqtd6a.webp', name: 'Nước giặt OMO Matic Hương Nước Hoa Comfort 4.1KG (túi)', link: 'https://s.shopee.vn/5q3rVWEVL0' },
+	{ src: 'https://down-tx-vn.img.susercontent.com/vn-11134207-81ztc-mlbihhl2e22t11.webp', name: 'Nước giặt xả TOPGIA hương lavender dịu nhẹ, tẩy sạch vết bẩn, lưu hương 72H, an toàn lành tính', link: 'https://s.shopee.vn/9ztQUproxc' },
+	{ src: 'https://down-tx-vn.img.susercontent.com/vn-11134207-7ra0g-m6xcgvjtar08ee.webp', name: 'Chảo Chống Dính Sâu Lòng Vân Đá Tổ Ong Được Cho Mọi Loại Bếp Chất Liệu Inox 3 Lớp Cao Cấp Tiện Dụng', link: 'https://s.shopee.vn/2qQFxnXg40' },
+	{ src: 'https://down-tx-vn.img.susercontent.com/vn-11134207-7r98o-lzfol6tlaobl4f.webp', name: 'Hộp Cơm Cắm Điện Hâm Nóng Inox Cao Cấp 3 Tầng Đa Năng Làm Nóng Nhanh An Toàn Chính Hãng', link: 'https://s.shopee.vn/9KdjhnlWZG' },
+	{ src: 'https://down-bs-vn.img.susercontent.com/73ce0c0d2e5e24e895882149a1e84c38.webp', name: 'Sáp thơm phòng khử mùi phòng ngủ nhà vệ sinh toilet ô tô 190g siêu tiện lợi GALA HOME', link: 'https://s.shopee.vn/8phTKkNDEC' }
+	
 ];
 
 // Icon mapping cho từng thể loại sách (dùng trong main.js)
